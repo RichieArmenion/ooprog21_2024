7b2286ca0906698f74f099fa47123ceec7c02a9f
Armenion28 <armenionrichieann98@gmail.com>
2024-08-14 08:24:38 -0700

    Update MovieQuoteInfor.java

--- Chapter 1/MovieQuoteInfor.java
+++ Chapter 1/MovieQuoteInfor.java
@@ -1,8 +1,7 @@
 public class MovieQuoteInfor {
    public static void main(String args[]) {
-      System.out.println("Huwag mo kong mahalin dahil mahal kita. Mahalin mo ako dahil mahal mo ako, because that is what I deserve.");
-      System.out.println("BARCELONA: A LOVE UNTOLD");
-      System.out.println("2016");
-       System.out.println("starring Kathryn Bernando and Daniel Padilla");
+      System.out.println("To infinity…and beyond!");
+      System.out.println("Toy Story");
+      System.out.println("1995");   
    }
 }
