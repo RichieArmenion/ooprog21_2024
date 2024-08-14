dce97289be69b15d3c9e825830963bf6e4ac28b7
Armenion28 <armenionrichieann98@gmail.com>
2024-08-14 23:14:31 +0800

    Add files via upload

---
+++ Chapter 1/MovieQuoteInfor.java
@@ -0,0 +1,8 @@
+public class MovieQuoteInfor {
+   public static void main(String args[]) {
+      System.out.println("Huwag mo kong mahalin dahil mahal kita. Mahalin mo ako dahil mahal mo ako, because that is what I deserve.");
+      System.out.println("BARCELONA: A LOVE UNTOLD");
+      System.out.println("2016");
+       System.out.println("starring Kathryn Bernando and Daniel Padilla");
+   }
+}
